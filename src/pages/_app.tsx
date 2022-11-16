d729e1a1dc93d6b59c245dbd5235bf42344d8354
xencodes <xencodes@icloud.com>
2022-11-16 09:01:14 +0530

    chore: add title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,10 +5,15 @@ import theme from '@/theme';
 import '@/styles/float.css';
 import '@fontsource/poppins/400.css';
 import '@fontsource/poppins/700.css';
+import Head from 'next/head';
+import { FIRST_NAME, LAST_NAME } from '@/config/profile';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<ChakraProvider theme={theme}>
+			<Head>
+				<title>{FIRST_NAME + ' ' + LAST_NAME}</title>
+			</Head>
 			<Component {...pageProps} />
 		</ChakraProvider>
 	);
